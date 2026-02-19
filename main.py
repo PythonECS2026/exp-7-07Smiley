7cb20d65a587542447198b7f4b5709cd9690c094
07Smiley <bhushanchavan782006@eng.rizvi.edu.in>
2026-02-19 23:50:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,40 @@
 # including their grades and attendance.
 # Coder:
 # Date:
-
-print("--- Student Records Manager ---")
-students_records = {
-    "251P005" : {"name":"Sameer","grade":"A","attendance": 68},
-    "251P055" : {"name":"Abdulla","grade":"B+","attendance": 88},
-    "251P026" : {"name":"Katrina","grade":"A-","attendance": 55},
+students = {
+    "251P005": {"name": "Sameer", "grade": "A", "attendance": 68},
+    "251P055": {"name": "Abdulla", "grade": "B+", "attendance": 88},
+    "251P026": {"name": "Katrina", "grade": "A-", "attendance": 55}
 }
 
-print(f"Current Student Records: {students_records}")
+print(f"Current Student Records: {students}")
+
+# add new student
+uin = input("Enter New Student UIN: ")
+name = input("Enter New Student Name: ")
+grade = input("Enter New Student Grade: ")
+attendance = int(input("Enter New Student Attendance: "))
+
+students[uin] = {
+    "name": name,
+    "grade": grade,
+    "attendance": attendance
+}
+
+# update grade
+update_uin = input("Enter UIN to Update: ")
+new_grade = input("Enter New Grade of Student: ")
+
+if update_uin in students:
+    students[update_uin]["grade"] = new_grade
+
+# delete student
+delete_uin = input("Enter UIN of the Student to Delete: ")
+
+if delete_uin in students:
+    del students[delete_uin]
+
+print(f"Final Student Records: {students}")
 
 # Write your code here
 # TODO: Add a new Student Record 
@@ -23,3 +48,4 @@ print(f"Current Student Records: {students_records}")
 
 # Stop coding here
 print(f"Final Student Records: {students_records}")
+
